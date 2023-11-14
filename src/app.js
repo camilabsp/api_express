cf9d43eeab5639359c39fd1b138f1b41dd4551aa
andresa <andresaacarol@gmail.com>
2023-11-14 15:42:19 -0300

    adding new things

--- src/app.js
+++ src/app.js
@@ -30,3 +30,57 @@ app.get('/filmes', (request, response) =>{
 app.listen(3000, () => {
     console.log('A API está funcionando!')
 })
+
+//Permite que os administradores atualizem informações sobre um filme existente no catálogo
+app.put('/filmes/:id', (request, response) => {
+    const indice = buscaIndiceFilme(request.params.id)
+    arquivoJson[indice].titulo = request.body.titulo
+    arquivoJson[indice].genero = request.body.genero
+    arquivoJson[indice].duracao = request.body.duracao
+    arquivoJson[indice].direcao = request.body.direcao
+    arquivoJson[indice].elenco = request.body.elenco
+    arquivoJson[indice].sinopse = request.body.sinopse
+    arquivoJson[indice].classificação = request.body.classificação
+    arquivoJson[indice].sessoes = request.body.sessoes
+    arquivoJson[indice].avaliação = request.body.avaliação
+    arquivoJson[indice].comentarios = request.body.comentarios
+    return response.send('Filme atualizado com sucesso!')
+}) 
+
+//Permite que os usuários atribuam uma avaliação (como uma pontuação de 1 a 5 estrelas) a um filme específico.
+app.post('/filmes/:id/avaliacao', (request, response) => {
+    const filmeId = parseInt(request.params.id);
+    const avaliacao = request.body.avaliacao;
+  
+    // Encontra o filme pelo Id
+    const filme = arquivoJson.find(filme => filme.id === filmeId);
+    
+    //Se não encontrar o Id retorna mensagem de erro
+    if (!filme) {
+      return response.status(404).send('Filme não encontrado.');
+    }
+  
+    // Adiciona a avaliação ao filme
+    filme.avaliacoes.push(avaliacao);
+  
+    return response.status(201).send('Avaliação adicionada com sucesso.');
+  });
+
+
+//Permite que os usuários publiquem um comentário sobre um filme específico.
+app.post('/filmes/:id/comentario', (request, response) => {
+    const filmeId = parseInt(request.params.id);
+    const comentario = request.body.comentario;
+  
+    // Encontra o filme pelo Id
+    const filme = arquivoJson.find(filme => filme.id === filmeId);
+    
+    //Se não encontrar o Id retorna mensagem de erro
+    if (!filme) {
+      return response.status(404).send('Filme não encontrado.');
+    }
+     // Adiciona a avaliação ao filme
+     filme.avaliacoes.push(avaliacao);
+  
+     return response.status(201).send('Avaliação adicionada com sucesso.');
+   });
